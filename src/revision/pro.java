469688908ce7169127b7b691fc12efbce66c45a5
hemanth965 <hk9262809@gmail.com>
2022-03-23 15:55:32 +0530

    commit message

--- src/revision/pro.java
+++ src/revision/pro.java
@@ -6,7 +6,7 @@ class sum {
     //prime
     public boolean prime(int n) {
         int count = 0;
-        for(int i = 2;i < n;i++) {
+        for(int i = 2;i/2 < n;i++) {
             if(n%i== 0)   {
                 count = 1;
             }
